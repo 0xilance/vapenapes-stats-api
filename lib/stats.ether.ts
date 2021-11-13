6073a98207a668c3a5682b512bc2e77e2678e15f
Andrew Miracle <me@andrewmiracle.com>
2021-11-13 02:29:00 +0000

    configure stats with infura

--- lib/stats.ether.ts
+++ lib/stats.ether.ts
@@ -2,17 +2,60 @@ import * as ethers from 'ethers'
 import { apes } from './apes.abi'
 import { Apes2981Instance } from './apes.interface';
 
-export const ROPSTEN = "";
-export const MAINNET = "";
+export const NETWORK = "ropsten";
+export const INFURA = "https://ropsten.infura.io/v3/c2659dd137f742b2b26c4a423d59895f"
 export const ADDRESS = "0xd4d4ab1bc9dbdbf55300af4e431d7fbca11a01dd"
 
-const providers = ethers.getDefaultProvider('ropsten')
+// Specify your own API keys
+// Each is optional, and if you omit it the default
+// API key for that service will be used.
+const providers = ethers.getDefaultProvider(NETWORK, {
+  // etherscan: YOUR_ETHERSCAN_API_KEY,
+  infura: "c2659dd137f742b2b26c4a423d59895f",
+  // Or if using a project secret:
+  // infura: {
+  //   projectId: YOUR_INFURA_PROJECT_ID,
+  //   projectSecret: YOUR_INFURA_PROJECT_SECRET,
+  // },
+  // alchemy: YOUR_ALCHEMY_API_KEY,
+  // pocket: YOUR_POCKET_APPLICATION_KEY
+  // Or if using an application secret key:
+  // pocket: {
+  //   applicationId: ,
+  //   applicationSecretKey:
+  // }
+});
+
 const contract = new ethers.Contract(ADDRESS, apes, providers) as unknown as Apes2981Instance
 
-export const _currentSupply = async () => await (await contract.totalSupply()).toString
-export const _price = async () => await (await contract.APE_PRICE()).toString
-export const _maxSupply = async () => await (await contract.maxApes()).toString
-export const _name = async () => await contract.name();
-export const _apesReveal = async () => contract.apesReveal();
-export const _revealTimeStamp = async () => (await contract.revealTimestamp()).toString
+export const _currentSupply = async () =>  {
+  const res = (await contract.totalSupply()).toString()
+  console.log(res)
+  return res
+}
+export const _price = async () => {
+  const res =  (await contract.APE_PRICE()).toString()
+  console.log(res)
+  return res;
+}
+export const _maxSupply = async () => {
+  const res =  (await contract.maxApes()).toString()
+  console.log(res)
+  return res
+}
+export const _name = async () => {
+  const res = await contract.name();
+  console.log(res)
+  return res;
+}
+export const _apesReveal = async () => {
+  const res =  await contract.apesReveal();
+  console.log(res)
+  return res
+}
+export const _revealTimeStamp = async () => {
+  const res =  await contract.revealTimestamp()
+  console.log(res)
+  return res
+}
 
--- pages/api/apes.ts
+++ pages/api/apes.ts
@@ -1,19 +1,23 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import {_name} from '@/lib/stats.ether'
+import {_name, _maxSupply, _price, _currentSupply, _revealTimeStamp, _apesReveal} from '@/lib/stats.ether'
 
 export default async function userHandler(req: NextApiRequest, res: NextApiResponse) {
   const { method } = req
 
-  const _getResponse = async () => {
-    return {
-      name: "holla"
-    }
+  const payload = {
+    name: await _name(),
+    price: await _price(),
+    maxSupply: await _maxSupply(),
+    currentSupply: await _currentSupply(),
+    revealTimeStamp: await _revealTimeStamp(),
+    apesReveal: await _apesReveal(),
+
   }
 
   switch (method) {
     case 'GET':
       // Get data from your database
-      res.status(200).json(await _getResponse())
+      res.status(200).json(payload)
       break
     case 'POST':
       res.status(200).json({ })
